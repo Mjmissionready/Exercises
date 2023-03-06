65d3a5b3e731c0c653bfdc60bf923ebc52315a52
MJ Yoon <minjuy@missionreadyhq.com>
2023-03-06 19:41:29 +1300

    060323 ShoppingList

--- src/Countercomponents/Counter.js
+++
@@ -1,9 +0,0 @@
-export default function Counter({ count, updateCount }) {
-
-    return (
-        <div>
-            <div> Count: {count}</div>
-            <button conClick={updateCount}> Add count </button>
-        </div>
-    );
-}
--- src/Countercomponents/InputComponent.js
+++
@@ -1,8 +0,0 @@
-export default function InputComponent({ handleChange }) {
-    
-    return (
-        <div style={{ border: "6px solid orange" }}>
-            <input type="text" onChange={handleChange} />
-        </div>
-    );
-}
